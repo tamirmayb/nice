effa558ccdee18c1c22750e5aff63f6631db9e31
tamirmayblat <m_tamir2@yahoo.com>
2020-09-09 13:50:38 +0300

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# nice
+# Nice Home Assignment
+
+## Message Handling Server.
+
+Author: Tamir Mayblat, tamirmayb@gmail.com
+
+## Content
+
+## Prerequisites :
+
+* A working rabbitmq server with factory settings (localhost:5672, username: guest, password: guest)
+
+## How to use :
+
+* Start rabbitmq server. 
+* Start ConsumerApp (8081) and ProducerApp (8080) microservices.
+* Send post request to the shuffle ms - http://localhost:8080/nice/message. 
+
+* Example :  curl -d "input=5" -X POST http://localhost:8080/nice/message
+or use any rest client to send the requests.
+
+### Thanks
--- producer/src/main/java/com/nice/producer/controller/MyController.java
+++ producer/src/main/java/com/nice/producer/controller/MyController.java
@@ -1,13 +1,15 @@
 package com.nice.producer.controller;
 
-import com.nice.producer.dto.MessageType;
 import com.nice.producer.service.ProducerService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value="nice")
